af570fca69f8f058468ef2362f0e0c5e27dc7530
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-01-31 13:20:04 -0700

    whoah

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => { 
+  let docu = document.querySelector("h1");
+  docu.onmouseover = event =>{
+    const letters = "ABCEDFGHIJKLMNOPQRSTUVWXYZ"
+
+    let iterations = 0;
+    const interval = setInterval(()=>{
+    event.target.innerText = event.target.innerText.split("")
+      .map((letter,index)=> {
+        if(index < iterations){
+          return event.target.dataset.value[index];
+        }
+        return letters[Math.floor( Math.random() * 26 )]
+        })
+      .join("");
+      if(iterations >= event.target.innerText.length) clearInterval( interval) 
+      iterations++;
+    },30)
+
+  }
+  console.log(docu);
+//
+})
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 data-value="RAYMOND LEWIS"> RAYMOND LEWIS</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+body{
+  background-color:black
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1{
+  color:white;
+  font-size: 5rem;
+  font-family: "Space Mono", monospace;
 }
 </style>
